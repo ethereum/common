a0bc9b9cd078ef71b4a4d234eb7bb47bbe9c88fd
holgerd77 <Holger.Drewes@gmail.com>
2017-09-15 11:20:19 +0200

    Added badge for build status to README

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# SYNOPSIS
+
+[![Build Status](https://img.shields.io/travis/ethereumjs/common.svg?branch=master&style=flat-square)](https://travis-ci.org/ethereumjs/common)
+
 # COMMON
 Resources common to all Ethereum implementations
 
--- tests/index.js
+++ tests/index.js
@@ -1,7 +1,7 @@
 const tape = require('tape')
 const params = require('../index.js')
 
-tape('JSON validity', function (t) {
+tape('JSON validity/Param reachability', function (t) {
   
   t.test('should be able to read value from params.json', function (st) {
     st.equal(params.genesisHash.v, "0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3")
